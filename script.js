3c290dca3867f6bce12469d6e6b146213db931bb
Hanady94 <>
2021-05-31 14:18:41 +0000

    webpage using css

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let userName = prompt ('please enter your name');
+alert ('welcome to my webpage' + userName )
+let favPit = prompt ('please enter your fav pit')
+ 
+function printFulldata(name,favPitanimal)
+
+{
+ //console.log('Fulldata is',userName,favPit);
+
+document.write('Fulldata is'+ ' ' + userName +' ' + favPit );
+
+
+
+}
+printFulldata(userName ,favPit );
+
